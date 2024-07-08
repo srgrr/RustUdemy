3fb01c05a53fe10ca8ce61d5de379accd64f935b
Sergio Raccoon Guasch <sergio.rodriguez@streamsets.com>
2024-07-08 17:56:06 +0200

    Add exercise a part 2A

--- exercise_a/src/main.rs
+++ exercise_a/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-  let missiles = 8;
+  let mut missiles = 8;
   let ready = 2;
   println!("Firing {} of my {} missiles...", ready, missiles);
+  missiles -= ready;
+  println!("{} missiles left", missiles);
 }
